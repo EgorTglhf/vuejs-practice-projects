2f8ffcc998b00414852c677a52a379d713e75766
Egor Tykmanov <egor.tykmanov@glowbyteconsulting.com>
2023-03-14 01:00:09 +0300

    added fetched photos and upload file

--- gallery-app/src/pages/PhotosPage.vue
+++ gallery-app/src/pages/PhotosPage.vue
@@ -1,11 +1,39 @@
 <template>
   <v-main>
-    <div>PHOTOS</div>
+    <v-container>
+      <PhotoForm @addPhoto="addPhoto" />
+      <v-row>
+        <Photo v-for="photo in photos" :photo="photo" />
+      </v-row>
+    </v-container>
   </v-main>
 </template>
 
 <script>
-export default {}
+import Photo from '../components/photo/Photo.vue'
+import PhotoForm from '../components/photo/PhotoForm.vue'
+import axios from 'axios'
+import { toHandlers } from 'vue'
+
+export default {
+  components: { Photo, PhotoForm },
+  data: () => ({
+    photos: []
+  }),
+  mounted() {
+    this.fetchPhoto()
+  },
+  methods: {
+    fetchPhoto() {
+      axios
+        .get('https://jsonplaceholder.typicode.com/photos?_limit=10')
+        .then((response) => (this.photos = response.data))
+    },
+    addPhoto(photo) {
+      this.photos.push(photo)
+    }
+  }
+}
 </script>
 
 <style scoped></style>
