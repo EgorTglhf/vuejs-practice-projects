d41c48d9a3315f6ee7bd60b4d3f912a20e649a23
Egor Tykmanov <egor.tykmanov@glowbyteconsulting.com>
2023-03-20 00:45:14 +0300

    added card actions btn

--- gallery-app/src/components/photo/Photo.vue
+++ gallery-app/src/components/photo/Photo.vue
@@ -1,10 +1,14 @@
 <template>
   <v-col cols="4">
-    <v-card @click="openPhoto">
+    <v-card>
       <v-card-title>{{ photo.title }}</v-card-title>
-      <v-card-text>
+      <v-card-text @click="openPhoto">
         <v-img :src="photo.url" width="200" height="200" />
       </v-card-text>
+      <v-card-actions>
+        <v-btn color="green"><v-icon icon="mdi-checkbox-marked-circle"></v-icon>Primary</v-btn>
+        <v-btn class="ml-auto" color="red"><v-icon icon="mdi-cancel"></v-icon></v-btn>
+      </v-card-actions>
     </v-card>
   </v-col>
 </template>
