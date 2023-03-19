16cdcf4d4cc5b0bb9bb8cfb01f8cd94fe2c8c099
Egor Tykmanov <egor.tykmanov@glowbyteconsulting.com>
2023-03-19 23:17:39 +0300

    a little clean old code

--- gallery-app/src/pages/PhotosPage.vue
+++ gallery-app/src/pages/PhotosPage.vue
@@ -1,7 +1,7 @@
 <template>
   <v-main>
     <v-container>
-      <PhotoForm v-if="photos.length < 11" @addPhoto="addPhoto" />
+      <PhotoForm v-if="$store.getters.getAllPhotos.length < 11" />
       <div v-else>You cannot add more photos</div>
       <v-row>
         <Photo v-for="photo in $store.getters.getAllPhotos" :photo="photo" />
@@ -19,17 +19,11 @@ import { mapActions } from 'vuex'
 
 export default {
   components: { Photo, PhotoForm, PhotoDialog },
-  data: () => ({
-    photos: []
-  }),
   mounted() {
     this.fetchPhotos()
   },
   methods: {
     ...mapActions(['fetchPhotos']),
-    addPhoto(photo) {
-      this.photos.push(photo)
-    },
     openPhoto(photo) {
       this.currentPhoto = photo
       this.dialogVisible = true
